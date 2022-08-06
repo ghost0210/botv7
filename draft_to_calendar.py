875efde0f28fd7c535001d122f31147982b7d815
shadowbum <110494804+shadowbum@users.noreply.github.com>
2022-08-06 05:56:02 -0400

    Update draft_to_calendar.py

--- draft_to_calendar.py
+++ draft_to_calendar.py
@@ -41,7 +41,7 @@ async def send_calendar(moodle: str, user: str, passw: str, urls: list) -> list:
                     "index": 0,
                     "methodname": "core_calendar_submit_create_update_form",
                     "args": {
-                        "formdata": "id=0&userid={}&modulename=&instance=0&visible=1&eventtype=user&sesskey={}&_qf__core_calendar_local_event_forms_create=1&mform_showmore_id_general=1&name=Subidas&timestart[day]=6&timestart[month]=5&timestart[year]=2023&timestart[hour]=18&timestart[minute]=55&description[text]={}&description[format]=1&description[itemid]=940353303&location=&duration=0"
+                        "formdata": "id=0&userid={}&modulename=&instance=0&visible=1&eventtype=user&sesskey={}&_qf__core_calendar_local_event_forms_create=1&mform_showmore_id_general=1&name=Subidas&timestart[day]=6&timestart[month]=5&timestart[year]=2023&timestart[hour]=18&timestart[minute]=55&description[text]={}&description[format]=1&description[itemid]=940353303&location=&duration=30"
                     },
                 }
             ]
